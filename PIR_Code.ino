0548f751cc36625d9bc533d7ab41795af709439b
Rafiul Alam Rami <121849752+RafiulAlamRami@users.noreply.github.com>
2023-10-29 06:50:31 +0600

    Add files via upload

---
+++ PIR_Code.ino
@@ -0,0 +1,16 @@
+void setup() 
+{
+  pinMode(12, INPUT);  // make the 12 pin an INPUT:
+  pinMode(8, OUTPUT);  // make the 8 pin an OUTPUT:
+}
+void loop() 
+{
+  int PIR_State = digitalRead(12);  // read the input pin:
+
+  if(PIR_State == HIGH)   // Check if PIR Sensor Detected Motion
+  {
+    digitalWrite(8,HIGH);  // Turn ON LED
+  }
+  else
+  digitalWrite(8,LOW);  // Turn OFF LED
+}
